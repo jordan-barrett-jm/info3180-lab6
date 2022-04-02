08a04fb73bf34887a685de74acd66ce3816b8c8c
unknown <jordan.barrett.jm@gmail.com>
2022-04-02 16:52:32 -0500

    Added NewsList component

---
+++ src/components/NewsList.vue
@@ -0,0 +1,28 @@
+<template>
+ <ul class="news__list">
+    <li class="news__item">News item 1</li>
+    <li class="news__item">News item 2</li>
+    <li class="news__item">News item 3</li>
+ </ul>
+</template>
+<script>
+export default {
+        data() {
+        return {};
+    },
+    created() {
+        fetch('https://newsapi.org/v2/top-headlines?country=us',
+        {
+        headers: {
+            'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+        }
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        });
+    }
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script>
-export default {
-    data() {
-        return {
-            message: "Hello World! Welcome to VueJS"
-        };
-    },
-}
+  import NewsList from '@/components/NewsList.vue'
+  export default {
+      data() {
+          return {
+              message: "Hello World Welcome to VueJS"
+          };
+      },
+      components: { NewsList }
+  }
 </script>
 
 <template>
@@ -13,6 +15,7 @@ export default {
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
+        <NewsList />
       </div>
     </div>
 </template>
